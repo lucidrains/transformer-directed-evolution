03aedd1ad77aac3f7b46702974eae90eee050923
lucidrains <lucidrains@gmail.com>
2025-02-16 10:24:05 -0800

    for the sake of a toy task, will just have the evolution director attend to all the genomes of the population, the pooled population embed will then be concatted per parent pair for crossover mask, and the individual for mutations

--- transformer_directed_evolution/evolution_director.py
+++ transformer_directed_evolution/evolution_director.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from torch.nn import Module, ModuleList
 
 from einops import rearrange
-from einops.layers.torch import Rearrange
+from einops.layers.torch import Rearrange, Reduce
 
 from x_transformer import Encoder
 
@@ -23,6 +23,7 @@ class EvolutionDirector(Module):
     def __init__(
         self,
         dim_genome,
+        dim,
         population_size,
         num_parents = 2,
         transformer: Encoder
@@ -33,20 +34,33 @@ class EvolutionDirector(Module):
 
         super().__init__()
 
+        self.proj_genome_to_model = nn.Linear(dim_genome, dim)
+
         self.transformer = transformer
 
+        self.pool = Reduce('b n d -> b d', 'mean')
+
         self.pred_mutation = nn.Sequential(
-            nn.Linear(dim_genome, dim_genome * 3, bias = False),  # predict either -1, 0., 1. (binary encoding)
+            nn.Linear(dim_genome + dim, dim_genome * 3, bias = False),  # predict either -1, 0., 1. (binary encoding)
             Rearrange('... (d mutate) -> d mutate'),
             nn.Softmax(dim = -1)
         )
 
         self.pred_crossover_mask = nn.Sequential(
             Rearrange('parents ... d -> ... (parents d)'),
-            nn.Linear(num_parents * dim_genome, num_parents * dim_genome, bias = False),
+            nn.Linear(num_parents * dim_genome + dim, num_parents * dim_genome, bias = False),
             Rearrange('... (parents d) -> parents ... d')
             nn.Softmax(dim = 0)
         )
 
-    def forward(self, x):
+    def forward(self, genome_pool):
+
+        tokens = self.proj_genome_to_model(genome_pool)
+
+        attended_population = self.transformer(tokens)
+
+        pooled_population_embed = self.pool(attended_population)
+
+        # concat the pooled embed for the evolution director to make a decision on crossover mask or mutation
+
         return x
